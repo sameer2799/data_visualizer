dfcd76c981e0711611d7763c2754405dc9c18ffa
Sameer Singh <sameersingh2799@gmail.com>
2024-09-12 10:50:04 +0530

    Added authentication using auth0

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,17 @@ import './css/nav.css';
 import NavNotice from './NavNotice.jsx';
 import NavMessage from './NavMessage.jsx';
 import NavAvatar from './NavAvatar.jsx';
+import LoginButton from './LoginButton.jsx';
+import LogoutButton from './LogoutButton.jsx';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Nav() {
-  return (
+	const { isAuthenticated } = useAuth0();
+
+  	return (
     <nav className="header-nav ms-auto ">
     	 <ul className="d-flex align-items-center">
+			{isAuthenticated ? <LogoutButton /> : <LoginButton />}
     	 	<NavNotice />
     	 	<NavMessage  />
     	 	<NavAvatar />
